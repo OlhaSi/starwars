6f8912cb04884124eefa2bc07e6bd24b023c0d11
OlhaSi <liush.olga@gmail.com>
2021-09-09 00:47:43 +0200

    add reformatted header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import './header.css';
+import "./header.css";
 
 const Header = (props) => {
-    const {onPersonChange, onPlanetChange, onStarshipChange} = props;
-    return (
-        <div className="header">
-            <ul>
-                <li>
-                    <a onClick={onPersonChange} href="">Persons</a>
-                </li>
-                <li>
-                    <a onClick={onPlanetChange} href="">Planets</a>
-                </li>
-                <li>
-                    <a onClick={onStarshipChange} href="">Starships</a>
-                </li>
-            </ul>
-        </div>
-    );
+  const { onPersonChange, onPlanetChange, onStarshipChange } = props;
+  return (
+    <div className="header">
+      <ul>
+        <li>
+          <a onClick={onPersonChange} href="">
+            Persons
+          </a>
+        </li>
+        <li>
+          <a onClick={onPlanetChange} href="">
+            Planets
+          </a>
+        </li>
+        <li>
+          <a onClick={onStarshipChange} href="">
+            Starships
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
